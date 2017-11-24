52cba7314dbf9f2d95c7edc5aa8ebec56ef5e759
Johnny Chapman <macuser@Johnnys-Macbook-Air.local>
2017-11-24 12:03:09 -0600

    Sync with local

---
+++ Ohm's Law Calculator/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Ohm's Law Calculator
+//
+//  Created by Johnny Chapman on 10/30/17.
+//  Copyright © 2017 Christine Berger. All rights reserved.
+//
+
+import Foundation
---
+++ Ohm's Law Calculator/values.swift
@@ -0,0 +1,17 @@
+//
+//  Structs.swift
+//  Ohm's Law Calculator
+//
+//  Created by Johnny Chapman on 10/30/17.
+//  Copyright © 2017 Christine Berger. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    static let ceruleanBlue = UIColor(red: 0.0 / 255.0, green: 158.0 / 255.0, blue: 220.0 / 255.0, alpha: 1.0)
+}
+
+struct String {
+}
